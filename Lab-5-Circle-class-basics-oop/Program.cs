f102712936c5af8987843ddbfded739ee56dcb3e
Daniel Svensson <daniel.svensson682@gmail.com>
2022-09-15 12:55:50 +0200

    implemented Triangle area calculation, no loop for it for multiple triangles

--- Lab-5-Circle-class-basics-oop/Program.cs
+++ Lab-5-Circle-class-basics-oop/Program.cs
@@ -6,19 +6,37 @@ namespace Lab_5_Circle_class_basics_oop
     {
         static void Main(string[] args)
         {
+            int SwitchyChoise = 0;
             string UserInput = "";
             int radius;
+            int TriBase = 0;
+            int TriSide1 = 0;
+            int TriSide2 = 0;
             int CustomCounter = 0;
-            bool CustomRadius = false;
+            bool CustomRadius = false; 
             Circle CalcTheCircle = new Circle();
+            
             ConsoleKey UserChoice;
-            Console.WriteLine("Hello and welcome to the Circle creator");
 
-            Console.WriteLine("The Area of a Circle with the Radius of 5 is: " + CalcTheCircle.CalcCircle(5));
-            Console.Write("\n");  // using Console.Write(New Line) for the fun of it, instead of using empty Console.WriteLine();
-            Console.WriteLine("The Area of a Circle with the Radius of 6 is: " + CalcTheCircle.CalcCircle(6));
-            Console.Write("\n");
 
+            Console.WriteLine("Greeting's User! \nDo you want to get the radius of Circles, or do you want to calculate the area of a Triangle?");
+
+            do
+            {
+                Console.WriteLine("1:Circle\n2:Triangle");
+                UserInput = Console.ReadLine();
+            } while (int.TryParse(UserInput, out SwitchyChoise) is false && (SwitchyChoise != 1 || SwitchyChoise != 2 ));
+
+            switch (SwitchyChoise)
+            {
+                case 1:
+                    UserInput = ""; // emptying the choice prior to user choosing the radius
+                    Console.WriteLine("Hello and welcome to the Circle Radius Calculator");
+
+                    Console.WriteLine("The Area of a Circle with the Radius of 5 is: " + CalcTheCircle.CalcCircle(5));
+                    Console.Write("\n");  // using Console.Write(New Line) for the fun of it, instead of using empty Console.WriteLine();
+                    Console.WriteLine("The Area of a Circle with the Radius of 6 is: " + CalcTheCircle.CalcCircle(6));
+                    Console.Write("\n");
             do // asks user if they want to calculate a circle with another radius
             {
                 Console.WriteLine("Do you want to calculate a Circle with a diffrent radius? Y/N");
@@ -28,7 +46,6 @@ namespace Lab_5_Circle_class_basics_oop
 
             } while (UserChoice != ConsoleKey.Y && UserChoice != ConsoleKey.N);
 
-            
             if (CustomRadius == true) 
             {
                 do
@@ -46,12 +63,17 @@ namespace Lab_5_Circle_class_basics_oop
                     
                     Console.WriteLine("Do you want to calculate a Circle with a diffrent radius? Y/N");
                     UserChoice = Console.ReadKey(false).Key;
+                    while (UserChoice != ConsoleKey.Y && UserChoice != ConsoleKey.N) // input Error handeling
+                            {
+                                Console.WriteLine("Wrong input Detected!, Try Again Y/N \n");
+                                UserChoice = Console.ReadKey(false).Key;
+                            }
                     Console.Write("\n");
                     if (CustomRadius == true && UserChoice == ConsoleKey.N)
                     {
                         CustomRadius = false; // teminates the loop on user's request
                     }
-                    else
+                    else if (UserChoice == ConsoleKey.Y)
                     {
                     UserInput = ""; // resetting string input to loop again if they chose to do another
                     CustomCounter++;
@@ -65,6 +87,31 @@ namespace Lab_5_Circle_class_basics_oop
                 } while (CustomRadius == true);
             }
 
+                    break;
+                case 2:
+                    Console.WriteLine("Hello and welcome to the Triangle Area Calculator\n");
+                    do // checks to see that it's a whole number
+                    { 
+                    Console.WriteLine("Please enter the Base of the Triangle");
+                        UserInput = Console.ReadLine();
+                    } while(int.TryParse(UserInput, out TriBase) is false);
+                    UserInput = ""; // emptying the input string for use in the next input.
+                    do // checks to see that it's a whole number
+                    {
+                        Console.WriteLine("Please enter the First side of the Triangle");
+                        UserInput = Console.ReadLine();
+                    } while (int.TryParse(UserInput, out TriSide1) is false);
+                    UserInput = ""; // emptying the input string for use in the next input.
+                    do // checks to see that it's a whole number
+                    {
+                        Console.WriteLine("Please enter the Second side of the Triangle");
+                        UserInput = Console.ReadLine();
+                    } while (int.TryParse(UserInput, out TriSide2) is false);
+                    UserInput = ""; // emptying the input string for use in the next input.
+                    TriangleCalc CalcTheArea = new TriangleCalc(TriBase, TriSide1, TriSide2);
+                    Console.WriteLine("The area of your Triangle is: " + CalcTheArea.CalculateTriangle());
+                    break;
+            }
             Console.WriteLine("Thanks for using this program, and good day to you.");
 
         }
@@ -93,6 +140,27 @@ namespace Lab_5_Circle_class_basics_oop
                 return MadeCircle;
             }
 
+        }
+
+        class TriangleCalc
+        {
+            int _TriBase;
+            int _TriSide1;
+            int _TriSide2;
+            public TriangleCalc(int TriBase,int Triside1, int Triside2) // skipping zero paramater constructor. User input sides
+            {
+                this._TriBase = TriBase;
+                this._TriSide1 = Triside1;
+                this._TriSide2 = Triside2;
+            }
+
+
+            public Double CalculateTriangle() // using Heron's Formula, Area = √s(s−a)(s−b)(s−c)   s = semi-perimeter which is: (a + b + c)/2
+            {
+                float SemiAreaOfTriangle = (this._TriBase + this._TriSide1 + this._TriSide2) / 2; // calculate the Semi-perimeter
+                float AreaOfTriangle = SemiAreaOfTriangle*(SemiAreaOfTriangle - _TriBase)*(SemiAreaOfTriangle - _TriSide1)* (SemiAreaOfTriangle - _TriSide2); // calculate the full area
+                return Math.Sqrt(AreaOfTriangle);
+            }
         }
     }
 }
